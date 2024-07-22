a76153e13c22126b3e7102a1be2381ea4775a04d
ObernhumerMichael <michaelobernhumer@gmail.com>
2024-07-22 20:56:18 +0200

    [#3] added the creation of a ohclv pandas dataframe from a csv

--- tests/data_handler/test_historic_data.py
+++ tests/data_handler/test_historic_data.py
@@ -4,32 +4,30 @@ from py_trading_lib.data_handler.historic_data import HistoricData
 
 
 class TestHistoricData:
-    def __init__(self) -> None:
-        self.historic_data = HistoricData()
 
-    def test_get_data_from_file_len(self):
+    def test_get_from_csv_len(self):
         valid_file_len = 8640
 
-        klines = self.historic_data.get_data_from_file(
-            "../../example_klines/BTC_USDT.csv"
-        )
+        klines = HistoricData().get_from_csv("./example_klines/BTC_USDT.csv")
 
         assert len(klines) == valid_file_len
 
-    def test_get_data_from_file_columns(self):
+    def test_get_from_csv_columns(self):
         expected_columns = ["TIME", "OPEN", "HIGH", "LOW", "CLOSE", "VOLUME"]
-        klines = self.historic_data.get_data_from_file(
-            "../../example_klines/BTC_USDT.csv"
-        )
+        klines = HistoricData().get_from_csv("./example_klines/BTC_USDT.csv")
 
         columns = klines.columns.tolist()
 
         assert columns == expected_columns
 
-    def test_get_data_from_file_no_file(self):
+    def test_get_from_csv_no_file(self):
         with pytest.raises(FileNotFoundError):
-            self.historic_data.get_data_from_file("no_file.csv")
+            HistoricData().get_from_csv("./no_file.csv")
 
-    def test_get_data_from_file_wrong_format(self):
+    def test_get_from_csv_wrong_file_ending(self):
         with pytest.raises(ValueError):
-            self.historic_data.get_data_from_file("./wrong-file.csv")
+            HistoricData().get_from_csv("./tests/data_handler/wrong-file.txt")
+
+    def test_get_from_csv_wrong_format(self):
+        with pytest.raises(ValueError):
+            HistoricData().get_from_csv("./tests/data_handler/wrong-format.csv")
