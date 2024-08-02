b518eb179c4f8d9675734366e94f6283f274ab0b
ObernhumerMichael <michaelobernhumer@gmail.com>
2024-08-02 23:08:31 +0200

    [#19] feat: signals produce an error now if conditions hold non bool values

--- py_trading_lib/analysis/signals.py
+++ py_trading_lib/analysis/signals.py
@@ -1,23 +1,16 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from copy import deepcopy
+from typing import Any, List
 
 import pandas as pd
 
 
 class ISignal(ABC):
-    conditions: pd.DataFrame
+    _conditions: pd.DataFrame
 
     @abstractmethod
     def __init__(self, conditions: pd.DataFrame):
-        pass
-
-    @abstractmethod
-    def _calculate_signal(self) -> Any:
-        pass
-
-    @abstractmethod
-    def _sanity_checks(self) -> None:
-        pass
+        self._conditions = conditions
 
     def is_signal_true(self) -> pd.Series:
         self._sanity_checks()
@@ -25,6 +18,33 @@ class ISignal(ABC):
         result = self._try_calculate_signal()
 
         return result
+
+    @abstractmethod
+    def _sanity_checks(self) -> None:
+        self._check_df_not_empty(self._conditions)
+        self._check_df_only_bools(self._conditions)
+
+    def _check_df_not_empty(self, df: pd.DataFrame):
+        if len(df) == 0:
+            raise ValueError(
+                "The conditions must contain values otherwise a signal can not be created."
+            )
+
+    def _check_df_only_bools(self, df: pd.DataFrame):
+        cols = self._get_colum_names(df)
+        is_bool_mask = pd.DataFrame()
+
+        for col in cols:
+            col_bool_mask = df[col].map(type) == bool
+            is_bool_mask = pd.concat([is_bool_mask, col_bool_mask], axis=1)
+
+        is_only_bools = is_bool_mask.all(axis=None)
+
+        if is_only_bools == False:
+            raise ValueError("The conditions passed contain values other than bool.")
+
+    def _get_colum_names(self, df: pd.DataFrame) -> List[str]:
+        return df.columns.tolist()
 
     def _try_calculate_signal(self) -> pd.Series:
         try:
@@ -35,29 +55,23 @@ class ISignal(ABC):
             )
 
         if not isinstance(signal, pd.Series):
-            raise ValueError("The signal was not a pandas Series.")
+            raise ValueError(
+                "Someting went wrong during the calculation of the signal. The signal was not a pandas Series."
+            )
 
         return signal
 
-    def _check_conditions_not_empty(self):
-        if len(self.conditions) == 0:
-            raise ValueError(
-                "The conditions must contain values otherwise a signal can not be created."
-            )
+    @abstractmethod
+    def _calculate_signal(self) -> Any:
+        pass
 
 
 class SignalAllConditionsTrue(ISignal):
     def __init__(self, conditions: pd.DataFrame):
-        self.conditions = conditions
+        super().__init__(conditions)
 
     def _sanity_checks(self) -> None:
-        self._check_conditions_not_empty()
-
-    def _check_all_true(self, row: pd.Series):
-        if row.hasnans:
-            return False
-
-        return row.all()
+        return super()._sanity_checks()
 
     def _calculate_signal(self) -> Any:
-        return self.conditions.apply(self._check_all_true, axis=1)
+        return self._conditions.all(axis=1)
--- tests/analysis/test_signals.py
+++ tests/analysis/test_signals.py
@@ -19,19 +19,9 @@ def sample_conditions():
 @pytest.fixture
 def sample_broken_condition():
     conditions = {
-        "a": [None, False, None],
-        "b": [True, None, None],
-        "c": [True, False, None],
-    }
-    return pd.DataFrame(conditions)
-
-
-@pytest.fixture
-def sample_mixed_condition():
-    conditions = {
-        "a": [True, True, False, None, False, None],
-        "b": [True, False, False, True, None, None],
-        "c": [True, False, False, True, False, None],
+        "a": [None, False, True],
+        "b": [True, False, True],
+        "c": [True, False, True],
     }
     return pd.DataFrame(conditions)
 
@@ -46,11 +36,6 @@ def signal_all_conditions_true_broken_conditions(sample_broken_condition):
     return SignalAllConditionsTrue(sample_broken_condition)
 
 
-@pytest.fixture
-def signal_all_conditions_true_mixed_conditions(sample_mixed_condition):
-    return SignalAllConditionsTrue(sample_mixed_condition)
-
-
 class TestSignals:
 
     @pytest.mark.parametrize("signal", [SignalAllConditionsTrue(pd.DataFrame())])
@@ -58,21 +43,41 @@ class TestSignals:
         with pytest.raises(ValueError):
             signal.is_signal_true()
 
+    @pytest.mark.parametrize("signal", ["signal_all_conditions_true_broken_conditions"])
+    def test_sanity_check_fail(self, signal: ISignal, request):
+        signal = request.getfixturevalue(signal)
+
+        with pytest.raises(ValueError):
+            signal._sanity_checks()
+
+    @pytest.mark.parametrize("signal", ["signal_all_conditions_true_valid_conditions"])
+    def test_sanity_check_pass(self, signal: ISignal, request):
+        signal = request.getfixturevalue(signal)
+
+        signal._sanity_checks()
+
     @pytest.mark.parametrize(
         "signal, expected",
         [
             ("signal_all_conditions_true_valid_conditions", [True, False, False]),
-            ("signal_all_conditions_true_broken_conditions", [False, False, False]),
-            (
-                "signal_all_conditions_true_mixed_conditions",
-                [True, False, False, False, False, False],
-            ),
         ],
     )
-    def test_is_signal_true(self, signal: ISignal, expected: List[bool], request):
+    def test_is_signal_true_valid_conditions(
+        self, signal: ISignal, expected: List[bool], request
+    ):
         signal = request.getfixturevalue(signal)
 
         result = signal.is_signal_true()
         result = result.tolist()
 
         assert result == expected
+
+    @pytest.mark.parametrize(
+        "signal",
+        ["signal_all_conditions_true_broken_conditions"],
+    )
+    def test_is_signal_true_invalid_conditions(self, signal: ISignal, request):
+        signal = request.getfixturevalue(signal)
+
+        with pytest.raises(ValueError):
+            signal.is_signal_true()
