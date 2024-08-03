39b906761c6f5ca03fe7a523bb109351d8ca3da6
Michael Obernhumer <105451321+ObernhumerMichael@users.noreply.github.com>
2024-08-03 15:27:02 +0200

    feat: signals now raise an error if conditions contain non bool values (#23)

--- py_trading_lib/analysis/signals.py
+++ py_trading_lib/analysis/signals.py
@@ -1,63 +1,64 @@
 from abc import ABC, abstractmethod
-from typing import Any
 
 import pandas as pd
 
 
-class ISignal(ABC):
-    conditions: pd.DataFrame
-
+class Signal(ABC):
     @abstractmethod
     def __init__(self, conditions: pd.DataFrame):
-        pass
+        self._conditions = conditions
 
-    @abstractmethod
-    def _calculate_signal(self) -> Any:
-        pass
+    def calculate_signal(self) -> pd.Series:
+        self._sanity_checks()
+        signal = self._try_calculate_signal()
+        return signal
 
     @abstractmethod
     def _sanity_checks(self) -> None:
-        pass
+        self._check_df_not_empty(self._conditions)
+        self._check_df_only_bools(self._conditions)
 
-    def is_signal_true(self) -> pd.Series:
-        self._sanity_checks()
+    def _check_df_not_empty(self, df: pd.DataFrame) -> None:
+        if df.empty:
+            raise ValueError(
+                "The conditions must contain values, otherwise a signal cannot be created."
+            )
 
-        result = self._try_calculate_signal()
+    def _check_df_only_bools(self, df: pd.DataFrame) -> None:
+        column_types = df.dtypes
+        all_bools = column_types.eq(bool).all()
 
-        return result
+        if not all_bools:
+            raise ValueError("The conditions passed contain values other than bool.")
 
     def _try_calculate_signal(self) -> pd.Series:
         try:
             signal = self._calculate_signal()
         except Exception as e:
-            raise Exception(
-                "Someting went wrong during the calculation of the signal." + str(e)
-            )
-
-        if not isinstance(signal, pd.Series):
-            raise ValueError("The signal was not a pandas Series.")
+            raise RuntimeError(
+                "Something went wrong during the calculation of the signal."
+            ) from e
 
         return signal
 
-    def _check_conditions_not_empty(self):
-        if len(self.conditions) == 0:
-            raise ValueError(
-                "The conditions must contain values otherwise a signal can not be created."
-            )
+    @abstractmethod
+    def _calculate_signal(self) -> pd.Series:
+        pass
 
 
-class SignalAllConditionsTrue(ISignal):
+class SignalAllConditionsTrue(Signal):
     def __init__(self, conditions: pd.DataFrame):
-        self.conditions = conditions
+        super().__init__(conditions)
 
     def _sanity_checks(self) -> None:
-        self._check_conditions_not_empty()
+        return super()._sanity_checks()
 
-    def _check_all_true(self, row: pd.Series):
-        if row.hasnans:
-            return False
+    def _calculate_signal(self) -> pd.Series:
+        signal = self._conditions.all(axis=1)
+        return self._validate(signal)
 
-        return row.all()
-
-    def _calculate_signal(self) -> Any:
-        return self.conditions.apply(self._check_all_true, axis=1)
+    def _validate(self, signal: pd.Series | bool) -> pd.Series:
+        if isinstance(signal, pd.Series):
+            return signal
+        else:
+            raise TypeError("The calculted signal is not a pandas Series.")
